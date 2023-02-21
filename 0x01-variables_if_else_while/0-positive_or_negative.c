bb9243c87b17d5b426acaa23fa097b2d448895b3
Thulane <Thulanemakunyane@yahoo.com>
2023-02-21 10:11:22 -0800

    Task 0.Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,23 +6,29 @@
 /* betty style doc for function main goes here */
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 {
 	int n;
 
-srand(time(0));
-n = rand() RAND_MAX / 2;
-/* your code goes here */
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
+/* your code goes here */
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
 return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,7 @@
-0x01. C - Variables, if, else, while
+Project done during **FULL Stack Software Engineeringstudies** at **ALX School**. It aims to learn about arithmetic operators, relational operators, boolen operators, comments, declaring variables and loops in **C language**.
+
+##Technologies
+* Scripts written in Bash 4.3.11(1)
+* C Files are compiled using gcc 4.8.4
+* C Files are written according to the C90 standard
+* Tested on Ubuntu 14.04 LTS
